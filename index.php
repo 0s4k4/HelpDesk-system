22e5e9f91349473061e2b99e4703a552837f1be2
Jorge Luis <soulbosss@gmail.com>
2021-08-15 16:41:11 -0500

    se crea estructura y añade codigo de index

---
+++ index.php
@@ -0,0 +1,18 @@
+<p>Observa la adminsitracion de tickets y respuesta del equipo de soporte</p>
+
+<table id="listTickets" class="table table-bordered table-striped">
+    <thead>
+        <tr>
+            <th>S/N</th>
+            <th>Ticket ID</th>
+            <th>Subject</th>
+            <th>Departament</th>
+            <th>Created by</th>
+            <th>Created</th>
+            <th>status</th>
+            <th></th>
+            <th></th>
+            <th></th>
+        </tr>
+    </thead>
+</table>
